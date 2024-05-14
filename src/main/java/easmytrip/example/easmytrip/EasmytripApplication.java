8dfcc5ca54d58e3518381b122ade5b7047344d54
sakshi <sakshishubhangi13@gmail.com>
2024-05-15 02:44:01 +0530

    codeRefactor

---
+++ src/main/java/easmytrip/example/easmytrip/EasmytripApplication.java
@@ -0,0 +1,13 @@
+package easmytrip.example.easmytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasmytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasmytripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/easmytrip/example/easmytrip/EasmytripApplicationTests.java
@@ -0,0 +1,13 @@
+package easmytrip.example.easmytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasmytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
